24a34a34aa6cf2814467d110e82b4d96e72590f5
LagFer <drakein90@gmail.com>
2018-05-21 11:52:39 -0300

    actualizacion con ej 6 y 7

---
+++ Practica-aye2/src/aye2/practica6/Empleado.java
@@ -0,0 +1,27 @@
+package aye2.practica6;
+
+public class Empleado {
+	
+	private int numero;
+	private String nombre;
+	
+	public int getNumero() {
+		return numero;
+	}
+	
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+	
+	public String getNombre() {
+		return nombre;
+	}
+	
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	public void verDatos() {
+		System.out.println("Empleado nro " + );
+	}
+}
---
+++ Practica-aye2/src/aye2/practica6/Hora.java
@@ -0,0 +1,79 @@
+package aye2.practica6;
+
+public class Hora {
+	
+	private int horas;
+	private int minutos;
+	private int segundos;
+	
+	public Hora() {
+		
+		horas = 0;
+		minutos = 0;
+		segundos = 0;
+
+	}
+	
+	public Hora(int x, int y, int z) {
+		
+		horas = x;
+		minutos = y;
+		segundos = z;
+		corregirHora();
+	}
+	
+	private void corregirHora() {
+		
+		if (segundos < 0) {
+			segundos = 0;
+		}else {
+			if (segundos > 59) {
+				minutos = segundos / 60;
+				segundos = segundos % 60;
+			}
+		}
+		if (minutos < 0) {
+			minutos = 0;
+		}else {
+			if (minutos > 59) {
+				horas = horas / 60;
+				minutos = minutos % 60;
+			}
+		}
+		if (horas < 0) {
+			horas = 0;
+		}else {
+			if(horas > 23) {
+				horas = horas % 24;
+				minutos = 0;
+				segundos = 0;
+			}
+		}
+	}
+	
+	public String toString() {
+		return "Hora " + String.format("%02d", horas) + ":" + minutos + ":" + segundos;
+	}
+	
+	
+	public void clone(Hora h) {
+		segundos = h.segundos;
+		minutos = h.minutos;
+		horas = h.horas;
+		
+	}
+	
+	public void sumar(Hora h2) {
+		segundos += h2.segundos;
+		minutos += h2.minutos;
+		horas += h2.horas;
+		corregirHora();
+	}
+	
+	public void Hora sumar(Hora h1, Hora h2) {
+		Hora h3 = new Hora();
+		h3.clone(h1);
+		h3.sumar(h2);
+		return h3;
+	}
+}
