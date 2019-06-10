368d5eedd257c6adb20dfc1df6948a9521dd0c25
AluDoc <AluDoc@DESKTOP-8GNEA3F>
2019-06-10 11:28:35 -0300

    Pila Practica 8 actualizado

--- Practica-aye2/src/aye2/Practica8/Pila.java
+++ Practica-aye2/src/aye2/Practica8/Pila.java
@@ -1,20 +1,22 @@
 package aye2.Practica8;
 
-import java.util.ArrayList;
+import java.util.List;;
 
-public class Pila<A> implements iPila{
-	private ArrayList<A> pila;
+public class Pila<A> implements iPila<A>{
+	private List<A> pila;
 	
 	public void apilar(A n) {
 		pila.add(n);
 	}
+
+	public Boolean pilaVacia() {
+		return pila.isEmpty();
+	}
 	
 	public A desapilar() {
-		
+			return pila.get(0);
 	}
 	
-	public Boolean pilaVacia() {
-		
+	public Pila() {
 	}
-
 }
--- Practica-aye2/src/aye2/Practica8/PruebaPractica8.java
+++ Practica-aye2/src/aye2/Practica8/PruebaPractica8.java
@@ -4,10 +4,12 @@ import java.util.ArrayList;
 
 public class PruebaPractica8 {
 	public static void main(String arg[]) {
-		ArrayList<Medible> lista = new ArrayList<Medible>();
-		for(Medible l: lista) {
-			System.out.println((Integer) l);
+		//ArrayList<Medible> lista = new ArrayList<Medible>();
+		//for(Medible l: lista) {
+		//	System.out.println((Integer) l);
+		Pila p = new Pila();
+		int n = 10;
+		p.apilar(n);
+		System.out.println(p.desapilar());
 		}
-	}
-
 }
